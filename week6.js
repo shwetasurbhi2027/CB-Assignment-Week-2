b006ef8ed3906968c194af8cdbdadc39b91d1fd6
shwetasurbhi2027 <144616595+shwetasurbhi2027@users.noreply.github.com>
2023-12-31 00:06:29 +0530

    Update week6.js

--- week6.js
+++ week6.js
@@ -11,28 +11,5 @@ function validateForm(event) {
     }
 
     alert('Form submitted successfully!'); 
-    const apiKey = 'YOUR_API_KEY';
-const city = 'DELHI'; 
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-
-
-function fetchWeatherData() {
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            const weatherInfo = `
-                <p><strong>City:</strong> ${data.name}</p>
-                <p><strong>Temperature:</strong> ${data.main.temp} °C</p>
-                <p><strong>Weather:</strong> ${data.weather[0].description}</p>
-            `;
-            document.getElementById('weatherInfo').innerHTML = weatherInfo;
-        })
-        .catch(error => {
-            console.error('Error fetching weather data:', error);
-        });
 }
 
-
-window.onload = fetchWeatherData;
-
-}
