94957ba2dad50d959a1610d86477280182eedd5a
shwetasurbhi2027 <144616595+shwetasurbhi2027@users.noreply.github.com>
2023-12-30 23:19:44 +0530

    Create week6.js

---
+++ week6.js
@@ -0,0 +1,14 @@
+function validateForm(event) {
+    event.preventDefault();
+
+    
+    let emailInput = document.getElementById('email');
+    let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    
+    if (!emailRegex.test(emailInput.value)) {
+        alert('Please enter a valid email address.');
+        return;
+    }
+
+    alert('Form submitted successfully!'); 
+}
