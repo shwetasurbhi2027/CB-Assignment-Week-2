8b89ed38f9d55f23f08166479cdd1f5ef7c842f6
shwetasurbhi2027 <144616595+shwetasurbhi2027@users.noreply.github.com>
2023-12-31 00:10:47 +0530

    Update week6.js

--- week6.js
+++ week6.js
@@ -13,3 +13,25 @@ function validateForm(event) {
     alert('Form submitted successfully!'); 
 }
 
+        function fetchRandomQuote() {
+            fetch('https://api.quotable.io/random')
+                .then(response => response.json())
+                .then(data => {
+                    // Display the fetched quote information on the webpage
+                    const quoteContainer = document.getElementById('quoteContainer');
+                    const quoteInfo = `
+                        <blockquote>
+                            <p>${data.content}</p>
+                            <footer>${data.author}</footer>
+                        </blockquote>
+                    `;
+                    quoteContainer.innerHTML = quoteInfo;
+                })
+                .catch(error => {
+                    console.error('Error fetching quote:', error);
+                });
+        }
+
+        
+        window.onload = fetchRandomQuote;
+
